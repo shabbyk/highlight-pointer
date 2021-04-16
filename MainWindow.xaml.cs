2d04ba441924edf242f912ec1f660bc4ac11b8e1
Shahbaaz <shahbaazkhan@outlook.com>
2021-04-16 22:40:45 +0530

    - Init with simple ellipse around cursor

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace CursorHighlighter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool GetCursorPos(ref Win32Point pt);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartHighLight();
+        }
+
+        private void StartHighLight()
+        {
+            var t = new DispatcherTimer();
+            t.Interval = new TimeSpan(0,0,0,0,20);
+            t.Tick += new EventHandler(PaintCircle);
+            t.Start();
+        }
+
+        private void PaintCircle(object sender, EventArgs e)
+        {
+            var pt = GetMousePosition();
+            using(var g = Graphics.FromHwnd(IntPtr.Zero))
+            {
+                g.DrawEllipse(Pens.YellowGreen, pt.X, pt.Y, 20, 20);
+            }
+        }
+
+        public static System.Drawing.Point GetMousePosition()
+        {
+            var w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+
+            return new System.Drawing.Point(w32Mouse.X, w32Mouse.Y);
+        }
+    }
+}
